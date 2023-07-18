01d367ab733a0f1490ed74bebf5e3c894c3529e8
wage <131868291+wage666@users.noreply.github.com>
2023-07-18 18:08:49 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import torch
-from diffusers import StableDiffusionImg2ImgPipeline, SemanticSegmentationPipeline
+from torchvision import models
+from diffusers import StableDiffusionImg2ImgPipeline
 from PIL import Image
 
 import os
@@ -11,20 +12,20 @@ import random
 
 load_dotenv()
 TG_TOKEN = os.getenv('YOUR_TOKEN_HERE')
-MODEL_SEGMENTATION = 'your_segmentation_model_data_path'
 MODEL_GENERATION = 'your_generation_model_data_path'
 
 LOW_VRAM_MODE = (os.getenv('LOW_VRAM', 'true').lower() == 'true')
 revision = "fp16" if LOW_VRAM_MODE else None
 torch_dtype = torch.float16 if LOW_VRAM_MODE else None
 
-# Load the semantic segmentation pipeline
-segmentationPipeline = SemanticSegmentationPipeline.from_pretrained(MODEL_SEGMENTATION)
-segmentationPipeline = segmentationPipeline.to("cuda")
-
 # Load the image generation pipeline
 generationPipeline = StableDiffusionImg2ImgPipeline.from_pretrained(MODEL_GENERATION, revision=revision, torch_dtype=torch_dtype)
 generationPipeline = generationPipeline.to("cuda")
+
+# Load the DeepLab model
+model_segmentation = models.segmentation.deeplabv3_resnet101(pretrained=True)
+model_segmentation = model_segmentation.to("cuda")
+model_segmentation.eval()
 
 # Disable safety checker if wanted
 def dummy_checker(images, **kwargs):
@@ -47,9 +48,11 @@ def get_try_again_markup():
 # Generate and send photo based on text input
 async def generate_and_send_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     progress_msg = await update.message.reply_text("Generating image...", reply_to_message_id=update.message.message_id)
-    
+
     prompt = update.message.text
-    segmentation_result = segmentationPipeline(prompt)
+    
+    # Perform segmentation on the input image
+    segmentation_result = perform_segmentation(prompt)
     
     # Apply the segmentation mask to the generated image
     generated_image = generationPipeline(prompt=prompt)
@@ -61,18 +64,33 @@ async def generate_and_send_photo(update: Update, context: ContextTypes.DEFAULT_
 # Generate and send photo based on photo input
 async def generate_and_send_photo_from_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     progress_msg = await update.message.reply_text("Generating image...", reply_to_message_id=update.message.message_id)
-    
+
     photo_file = await update.message.photo[-1].get_file()
     photo = await photo_file.download_as_bytearray()
-    
-    segmentation_result = segmentationPipeline(photo)
-    
+
+    # Perform segmentation on the input photo
+    segmentation_result = perform_segmentation(photo)
+
     # Apply the segmentation mask to the generated image
     generated_image = generationPipeline(photo=photo)
     generated_image = generated_image * segmentation_result
-    
+
     await context.bot.delete_message(chat_id=progress_msg.chat_id, message_id=progress_msg.message_id)
     await context.bot.send_photo(update.effective_user.id, image_to_bytes(generated_image), reply_markup=get_try_again_markup(), reply_to_message_id=update.message.message_id)
+
+# Perform segmentation on the input image
+def perform_segmentation(image):
+    input_tensor = preprocess_image(image).to("cuda")
+    with torch.no_grad():
+        output = model_segmentation(input_tensor)["out"]
+    masks = torch.argmax(output.squeeze(), dim=0).cpu().numpy()
+    return masks
+
+# Preprocess the input image
+def preprocess_image(image):
+    image = Image.open(BytesIO(image))
+    # Add preprocessing steps if needed
+    return image
 
 # Handle button callbacks
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:

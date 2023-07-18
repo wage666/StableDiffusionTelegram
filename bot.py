eb8cea2540c824d851be358581de89440ce09539
wage <131868291+wage666@users.noreply.github.com>
2023-07-18 17:39:47 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import torch
-from torch import autocast
-from diffusers import StableDiffusionPipeline, StableDiffusionImg2ImgPipeline
+from diffusers import StableDiffusionImg2ImgPipeline, SemanticSegmentationPipeline
 from PIL import Image
 
 import os
@@ -11,37 +10,27 @@ from io import BytesIO
 import random
 
 load_dotenv()
-TG_TOKEN = os.getenv('TG_TOKEN')
-MODEL_DATA = os.getenv('MODEL_DATA', 'runwayml/stable-diffusion-v1-5')
-LOW_VRAM_MODE = (os.getenv('LOW_VRAM', 'true').lower() == 'true')
-USE_AUTH_TOKEN = (os.getenv('USE_AUTH_TOKEN', 'true').lower() == 'true')
-SAFETY_CHECKER = (os.getenv('SAFETY_CHECKER', 'true').lower() == 'true')
-HEIGHT = int(os.getenv('HEIGHT', '512'))
-WIDTH = int(os.getenv('WIDTH', '512'))
-NUM_INFERENCE_STEPS = int(os.getenv('NUM_INFERENCE_STEPS', '50'))
-STRENGTH = float(os.getenv('STRENGTH', '0.75'))
-GUIDANCE_SCALE = float(os.getenv('GUIDANCE_SCALE', '7.5'))
+TG_TOKEN = os.getenv('YOUR_TOKEN_HERE')
+MODEL_SEGMENTATION = 'your_segmentation_model_data_path'
+MODEL_GENERATION = 'your_generation_model_data_path'
 
+LOW_VRAM_MODE = (os.getenv('LOW_VRAM', 'true').lower() == 'true')
 revision = "fp16" if LOW_VRAM_MODE else None
 torch_dtype = torch.float16 if LOW_VRAM_MODE else None
 
-# load the text2img pipeline
-pipe = StableDiffusionPipeline.from_pretrained(MODEL_DATA, use_auth_token=USE_AUTH_TOKEN)
-pipe = pipe.to("cuda")
+# Load the semantic segmentation pipeline
+segmentationPipeline = SemanticSegmentationPipeline.from_pretrained(MODEL_SEGMENTATION)
+segmentationPipeline = segmentationPipeline.to("cuda")
 
-# load the img2img pipeline
-img2imgPipe = StableDiffusionImg2ImgPipeline.from_pretrained(MODEL_DATA, use_auth_token=USE_AUTH_TOKEN)
-img2imgPipe = img2imgPipe.to("cuda")
+# Load the image generation pipeline
+generationPipeline = StableDiffusionImg2ImgPipeline.from_pretrained(MODEL_GENERATION, revision=revision, torch_dtype=torch_dtype)
+generationPipeline = generationPipeline.to("cuda")
 
-# disable safety checker if wanted
+# Disable safety checker if wanted
 def dummy_checker(images, **kwargs):
     return images, False
 
-if not SAFETY_CHECKER:
-    pipe.safety_checker = dummy_checker
-    img2imgPipe.safety_checker = dummy_checker
-
-
+# Image to bytes conversion function
 def image_to_bytes(image):
     bio = BytesIO()
     bio.name = 'image.jpeg'
@@ -49,80 +38,51 @@ def image_to_bytes(image):
     bio.seek(0)
     return bio
 
-
+# Function to get the "Try again" and "Variations" inline keyboard markup
 def get_try_again_markup():
-    keyboard = [
-        [InlineKeyboardButton("Try again", callback_data="TRYAGAIN"), InlineKeyboardButton("Variations", callback_data="VARIATIONS")]
-    ]
+    keyboard = [[InlineKeyboardButton("Try again", callback_data="TRYAGAIN"), InlineKeyboardButton("Variations", callback_data="VARIATIONS")]]
     reply_markup = InlineKeyboardMarkup(keyboard)
     return reply_markup
 
-
-def generate_image_with_clothing_mask(prompt, seed=None, height=HEIGHT, width=WIDTH, num_inference_steps=NUM_INFERENCE_STEPS, strength=STRENGTH, guidance_scale=GUIDANCE_SCALE, photo=None):
-    seed = seed if seed is not None else random.randint(1, 10000)
-    generator = torch.cuda.manual_seed_all(seed)
-
-    # Generate the image with clothing mask
-    pipe.to("cpu")
-    img2imgPipe.to("cuda")
-    init_image = Image.open(BytesIO(photo)).convert("RGB")
-    init_image = init_image.resize((height, width))
-    with autocast("cuda"):
-        result = img2imgPipe(prompt=[prompt], init_image=init_image, generator=generator, strength=strength, guidance_scale=guidance_scale, num_inference_steps=num_inference_steps)
-    
-    image = result["images"][0]
-    mask = result["masks"][0]
-
-    # Apply the clothing mask to the generated image
-    image = torch.where(mask > 0.5, image, init_image)
-
-    return image, seed
-
-
+# Generate and send photo based on text input
 async def generate_and_send_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     progress_msg = await update.message.reply_text("Generating image...", reply_to_message_id=update.message.message_id)
-    im, seed = generate_image_with_clothing_mask(prompt=update.message.text)
+    
+    prompt = update.message.text
+    segmentation_result = segmentationPipeline(prompt)
+    
+    # Apply the segmentation mask to the generated image
+    generated_image = generationPipeline(prompt=prompt)
+    generated_image = generated_image * segmentation_result
+    
     await context.bot.delete_message(chat_id=progress_msg.chat_id, message_id=progress_msg.message_id)
-    await context.bot.send_photo(update.effective_user.id, image_to_bytes(im), caption=f'"{update.message.text}" (Seed: {seed})', reply_markup=get_try_again_markup(), reply_to_message_id=update.message.message_id)
+    await context.bot.send_photo(update.effective_user.id, image_to_bytes(generated_image), caption=f'"{prompt}"', reply_markup=get_try_again_markup(), reply_to_message_id=update.message.message_id)
 
-
+# Generate and send photo based on photo input
 async def generate_and_send_photo_from_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if update.message.caption is None:
-        await update.message.reply_text("The photo must contain a text in the caption", reply_to_message_id=update.message.message_id)
-        return
     progress_msg = await update.message.reply_text("Generating image...", reply_to_message_id=update.message.message_id)
+    
     photo_file = await update.message.photo[-1].get_file()
     photo = await photo_file.download_as_bytearray()
-    im, seed = generate_image_with_clothing_mask(prompt=update.message.caption, photo=photo)
+    
+    segmentation_result = segmentationPipeline(photo)
+    
+    # Apply the segmentation mask to the generated image
+    generated_image = generationPipeline(photo=photo)
+    generated_image = generated_image * segmentation_result
+    
     await context.bot.delete_message(chat_id=progress_msg.chat_id, message_id=progress_msg.message_id)
-    await context.bot.send_photo(update.effective_user.id, image_to_bytes(im), caption=f'"{update.message.caption}" (Seed: {seed})', reply_markup=get_try_again_markup(), reply_to_message_id=update.message.message_id)
+    await context.bot.send_photo(update.effective_user.id, image_to_bytes(generated_image), reply_markup=get_try_again_markup(), reply_to_message_id=update.message.message_id)
 
-
+# Handle button callbacks
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     query = update.callback_query
     replied_message = query.message.reply_to_message
 
     await query.answer()
     progress_msg = await query.message.reply_text("Generating image...", reply_to_message_id=replied_message.message_id)
-
-    if query.data == "TRYAGAIN":
-        if replied_message.photo is not None and len(replied_message.photo) > 0 and replied_message.caption is not None:
-            photo_file = await replied_message.photo[-1].get_file()
-            photo = await photo_file.download_as_bytearray()
-            prompt = replied_message.caption
-            im, seed = generate_image_with_clothing_mask(prompt, photo=photo)
-        else:
-            prompt = replied_message.text
-            im, seed = generate_image_with_clothing_mask(prompt)
-    elif query.data == "VARIATIONS":
-        photo_file = await query.message.photo[-1].get_file()
-        photo = await photo_file.download_as_bytearray()
-        prompt = replied_message.text if replied_message.text is not None else replied_message.caption
-        im, seed = generate_image_with_clothing_mask(prompt, photo=photo)
-    
     await context.bot.delete_message(chat_id=progress_msg.chat_id, message_id=progress_msg.message_id)
-    await context.bot.send_photo(update.effective_user.id, image_to_bytes(im), caption=f'"{prompt}" (Seed: {seed})', reply_markup=get_try_again_markup(), reply_to_message_id=replied_message.message_id)
-
+    await context.bot.send_message(update.effective_user.id, "Sorry, image generation variations are not available.")
 
 app = ApplicationBuilder().token(TG_TOKEN).build()
 

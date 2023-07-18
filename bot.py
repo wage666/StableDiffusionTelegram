da1d59a97906e52f7b3341acacabcd02f4e575e7
wage <131868291+wage666@users.noreply.github.com>
2023-07-18 09:32:06 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ SAFETY_CHECKER = (os.getenv('SAFETY_CHECKER', 'true').lower() == 'true')
 HEIGHT = int(os.getenv('HEIGHT', '512'))
 WIDTH = int(os.getenv('WIDTH', '512'))
 NUM_INFERENCE_STEPS = int(os.getenv('NUM_INFERENCE_STEPS', '50'))
-STRENTH = float(os.getenv('STRENTH', '0.75'))
+STRENGTH = float(os.getenv('STRENGTH', '0.75'))
 GUIDANCE_SCALE = float(os.getenv('GUIDANCE_SCALE', '7.5'))
 
 revision = "fp16" if LOW_VRAM_MODE else None
@@ -34,7 +34,9 @@ img2imgPipe = StableDiffusionImg2ImgPipeline.from_pretrained(MODEL_DATA, revisio
 img2imgPipe = img2imgPipe.to("cpu")
 
 # disable safety checker if wanted
-def dummy_checker(images, **kwargs): return images, False
+def dummy_checker(images, **kwargs):
+    return images, False
+
 if not SAFETY_CHECKER:
     pipe.safety_checker = dummy_checker
     img2imgPipe.safety_checker = dummy_checker
@@ -47,46 +49,42 @@ def image_to_bytes(image):
     bio.seek(0)
     return bio
 
+
 def get_try_again_markup():
-    keyboard = [[InlineKeyboardButton("Try again", callback_data="TRYAGAIN"), InlineKeyboardButton("Variations", callback_data="VARIATIONS")]]
+    keyboard = [
+        [InlineKeyboardButton("Try again", callback_data="TRYAGAIN"), InlineKeyboardButton("Variations", callback_data="VARIATIONS")]
+    ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     return reply_markup
 
 
-def generate_image(prompt, seed=None, height=HEIGHT, width=WIDTH, num_inference_steps=NUM_INFERENCE_STEPS, strength=STRENTH, guidance_scale=GUIDANCE_SCALE, photo=None):
+def generate_image_with_clothing_mask(prompt, seed=None, height=HEIGHT, width=WIDTH, num_inference_steps=NUM_INFERENCE_STEPS, strength=STRENGTH, guidance_scale=GUIDANCE_SCALE, photo=None):
     seed = seed if seed is not None else random.randint(1, 10000)
     generator = torch.cuda.manual_seed_all(seed)
 
-    if photo is not None:
-        pipe.to("cpu")
-        img2imgPipe.to("cuda")
-        init_image = Image.open(BytesIO(photo)).convert("RGB")
-        init_image = init_image.resize((height, width))
-        with autocast("cuda"):
-            image = img2imgPipe(prompt=[prompt], init_image=init_image,
-                                    generator=generator,
-                                    strength=strength,
-                                    guidance_scale=guidance_scale,
-                                    num_inference_steps=num_inference_steps)["images"][0]
-    else:
-        pipe.to("cuda")
-        img2imgPipe.to("cpu")
-        with autocast("cuda"):
-            image = pipe(prompt=[prompt],
-                                    generator=generator,
-                                    strength=strength,
-                                    height=height,
-                                    width=width,
-                                    guidance_scale=guidance_scale,
-                                    num_inference_steps=num_inference_steps)["images"][0]
+    # Generate the image with clothing mask
+    pipe.to("cpu")
+    img2imgPipe.to("cuda")
+    init_image = Image.open(BytesIO(photo)).convert("RGB")
+    init_image = init_image.resize((height, width))
+    with autocast("cuda"):
+        result = img2imgPipe(prompt=[prompt], init_image=init_image, generator=generator, strength=strength, guidance_scale=guidance_scale, num_inference_steps=num_inference_steps)
+    
+    image = result["images"][0]
+    mask = result["masks"][0]
+
+    # Apply the clothing mask to the generated image
+    image = torch.where(mask > 0.5, image, init_image)
+
     return image, seed
 
 
 async def generate_and_send_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     progress_msg = await update.message.reply_text("Generating image...", reply_to_message_id=update.message.message_id)
-    im, seed = generate_image(prompt=update.message.text)
+    im, seed = generate_image_with_clothing_mask(prompt=update.message.text)
     await context.bot.delete_message(chat_id=progress_msg.chat_id, message_id=progress_msg.message_id)
     await context.bot.send_photo(update.effective_user.id, image_to_bytes(im), caption=f'"{update.message.text}" (Seed: {seed})', reply_markup=get_try_again_markup(), reply_to_message_id=update.message.message_id)
+
 
 async def generate_and_send_photo_from_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if update.message.caption is None:
@@ -95,7 +93,7 @@ async def generate_and_send_photo_from_photo(update: Update, context: ContextTyp
     progress_msg = await update.message.reply_text("Generating image...", reply_to_message_id=update.message.message_id)
     photo_file = await update.message.photo[-1].get_file()
     photo = await photo_file.download_as_bytearray()
-    im, seed = generate_image(prompt=update.message.caption, photo=photo)
+    im, seed = generate_image_with_clothing_mask(prompt=update.message.caption, photo=photo)
     await context.bot.delete_message(chat_id=progress_msg.chat_id, message_id=progress_msg.message_id)
     await context.bot.send_photo(update.effective_user.id, image_to_bytes(im), caption=f'"{update.message.caption}" (Seed: {seed})', reply_markup=get_try_again_markup(), reply_to_message_id=update.message.message_id)
 
@@ -112,18 +110,18 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
             photo_file = await replied_message.photo[-1].get_file()
             photo = await photo_file.download_as_bytearray()
             prompt = replied_message.caption
-            im, seed = generate_image(prompt, photo=photo)
+            im, seed = generate_image_with_clothing_mask(prompt, photo=photo)
         else:
             prompt = replied_message.text
-            im, seed = generate_image(prompt)
+            im, seed = generate_image_with_clothing_mask(prompt)
     elif query.data == "VARIATIONS":
         photo_file = await query.message.photo[-1].get_file()
         photo = await photo_file.download_as_bytearray()
         prompt = replied_message.text if replied_message.text is not None else replied_message.caption
-        im, seed = generate_image(prompt, photo=photo)
+        im, seed = generate_image_with_clothing_mask(prompt, photo=photo)
+    
     await context.bot.delete_message(chat_id=progress_msg.chat_id, message_id=progress_msg.message_id)
     await context.bot.send_photo(update.effective_user.id, image_to_bytes(im), caption=f'"{prompt}" (Seed: {seed})', reply_markup=get_try_again_markup(), reply_to_message_id=replied_message.message_id)
-
 
 
 app = ApplicationBuilder().token(TG_TOKEN).build()
